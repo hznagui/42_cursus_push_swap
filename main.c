8dbcc249b15000439841ff1cafa97bc77cbb1e89
Houdaifa Znagui <hznagui@e2r12p1.1337.ma>
2022-12-22 19:08:50 +0100

    done

--- main.c
+++ main.c
@@ -6,76 +6,77 @@
 /*   By: hznagui <hznagui@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/19 12:31:40 by hznagui           #+#    #+#             */
-/*   Updated: 2022/12/22 17:49:13 by hznagui          ###   ########.fr       */
+/*   Updated: 2022/12/22 18:07:13 by hznagui          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 #include <unistd.h>
 
-int ft_error(char **argc, int argv)
+void	abort(void)
 {
-    int i;
-    int a;
-    
-    a = 1;
-    i = 0;
-    while(a < argv)
-    {
-       while(argc[a][i] != '\0')
-       {
-           if ((argc[a][i] >= 64 && argc[a][i] <= 127) || (argc[a][i] >= 33 && argc[a][i] <= 44) || (argc[a][i] >= '.' && argc[a][i] <= '/') || (argc[a][i] == '-' && (argc[a][i + 1] > '9' || argc[a][i + 1] < '0' || argc[a][i - 1] != '\0' || argc[a][i - 1] != ' ')) )
-               return(1);
-           i++;        
-       }
-       i = 0;
-       a++;
-    }
-return (0);
+	write(2, "Error\n", 6);
+	exit (1);
 }
-int ft_check_double(t_list *p)
+
+void ft_error(char **argv, int argc)
 {
-    t_list *h;
-    t_list *o;
-    
-    h = p;
-    while (h)
-    {
-        if ((long long)h->content > INT_MAX || (long long)h->content < INT_MIN)
-            return(1);
-        o = h->next;
-        while (o)
-        {
-            if (o->content == h->content)
-                return(1);
-            else
-            {
-                o = o->next;
-            }
-        }
-        h = h->next;
-    }
-    return (0);
+	int i;
+	int a;
+
+	a = 1;
+	i = 0;
+	while(a < argc)
+	{
+		if(argv[a][0] == '\0')
+			abort();
+		while(argv[a][i] != '\0')
+		{
+			if ((argv[a][i] >= 64 && argv[a][i] <= 127) || (argv[a][i] >= 33 && argv[a][i] <= 44) || (argv[a][i] >= '.' && argv[a][i] <= '/') || (argv[a][i] == '-' && (argv[a][i + 1] > '9' || argv[a][i + 1] < '0' || argv[a][i - 1] != '\0' || argv[a][i - 1] != ' '))  )
+				abort();
+			i++;
+	   }
+	   i = 0;
+	   a++;
+	}
 }
+
+void ft_check_double(t_list *p)
+{
+	t_list *h;
+	t_list *o;
+
+	h = p;
+	while (h)
+	{
+		if ((long long)h->content > INT_MAX || (long long)h->content < INT_MIN)
+			abort();
+		o = h->next;
+		while (o)
+		{
+			if (o->content == h->content)
+				abort();
+			else
+			{
+				o = o->next;
+			}
+		}
+		h = h->next;
+	}
+}
+
 int main(int argc,char **argv)
 {
-    if (argc == 1)
-        return (0);
-    if (ft_error(argv,argc) == 1) {
-        write(2, "Error\n", 6);
-        return(-1);
-    }
-			t_list *d;
+	if (argc == 1)
+		return (0);
+	ft_error(argv,argc);
+	t_list *d;
 	d = ft_split(argv,' ');
-    if (ft_check_double(d) == 1) {
-        write(2, "Error\n", 6);
-        return(-1);
-    }
+	ft_check_double(d);
 	while (d)
 	{
 		printf("%d\n",(int)d->content);
 		d=d->next;
 	}
-    return (0);
-}//200000000000 
- //2147483647
+	return (0);
+}

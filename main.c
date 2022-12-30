415c472cdc31731aad01a85e46df98cddb5c3cb7
Houdaifa Znagui <hznagui@e2r9p9.1337.ma>
2022-12-30 16:42:36 +0100

    done

--- main.c
+++ main.c
@@ -6,23 +6,56 @@
 /*   By: hznagui <hznagui@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/19 12:31:40 by hznagui           #+#    #+#             */
-/*   Updated: 2022/12/30 14:30:22 by hznagui          ###   ########.fr       */
+/*   Updated: 2022/12/30 16:39:11 by hznagui          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+t_list	*ft_lstlast(t_list *lst)
+{
+	t_list	*p;
+
+	p = lst;
+	if (!lst)
+		return (NULL);
+	while (p -> next)
+	{
+		p = p -> next;
+	}
+	return (p);
+}
 
 void ft_case_3(t_list **stack1, t_list **stack2)
-{ 
-	while(*stack1)
+{
+	int p1;
+	int p2;
+	int oldp1;
+	p1 = 0;
+	p2 = 0;
+	ft_index(stack1);
+	while (ft_lstsize(*stack1)>3)
 	{
-		push_the_smallest(stack1,stack2);
+		oldp1 = p1;
+		p1 += ft_lstsize(*stack1)/3;
+		p2 = p1 - ((p1 - oldp1) / 2);
+		while (ft_lstsize(*stack2) < p1)
+		{	
+			if ((int)(*stack1)->index <= p1)
+			{
+				ft_pb(stack2,stack1);
+				if ((int)(*stack2)->index <= p2)
+				{
+					ft_rb(stack2);
+				}
+			}
+			else
+			{
+				ft_ra(stack1);
+			}
+		}
 	}
-	while (*stack2)
-	{	
-	ft_pb(stack2,stack1);
-	}
+	ft_case_1(stack1);
 }
 int main(int argc,char **argv)
 {
@@ -33,24 +66,19 @@ int main(int argc,char **argv)
 	ft_error(argv,argc);
 	stack1 = ft_split(argv,' ');
 	ft_check_double(stack1);
-	ft_case_2(&stack1,&stack2);
-	ft_index(&stack1);
-	// ft_pb(&stack2,&stack1);
-	// ft_pa(&stack1,&stack2);
-
-	
-	
-	while (stack1)
-	{
-		printf("\n%d",(int)stack1->content);
-		stack1 = stack1->next;
-	}
+	ft_case_3(&stack1,&stack2);
+	// ft_index(&stack1);
+	// while (stack1)
+	// {
+	// 	printf("\n%d",(int)stack1->index);
+	// 	stack1 = stack1->next;
+	// }
 	// printf("\ntisa3\n");
-	while (stack2)
-	{
-		printf("--%d--\n",(int)stack2->content);
-		stack2 = stack2->next;
-	}
+	// while (stack2)
+	// {
+	// 	printf("--%d--\n",(int)stack2->content);
+	// 	stack2 = stack2->next;
+	// }
 	
 	
 	return (0);
